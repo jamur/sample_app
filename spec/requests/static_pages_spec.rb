3aa5ec268a5fa705d0b4860bd97fa5462ddc2626
Rafael Jamur <jamur@ufpr.br>
2014-06-10 19:10:18 -0300

    Finish HALF layout and routes

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -5,17 +5,17 @@ describe "StaticPages" do
 	describe "Home page" do
 
 		it "should have the content 'Sample App'" do
-			visit '/static_pages/home'
+			visit root_path
 			expect(page).to have_content('Sample App')
 		end
 
 		it "should have the base title" do
-			visit '/static_pages/home'
+			visit root_path
 			expect(page).to have_title("Ruby on Rails Tutorial Sample App")
 		end
 
 		it "should not have a custom page title" do
-			visit '/static_pages/home'
+			visit root_path
 			expect(page).not_to have_title("| Home")
 		end
 	end
@@ -23,12 +23,12 @@ describe "StaticPages" do
 	describe "Help page" do
 
 		it "should have the content 'Help'" do
-			visit '/static_pages/help'
+			visit help_path
 			expect(page).to have_content('Help')
 		end
 
 		it "should have the title 'Help'" do
-			visit '/static_pages/help'
+			visit help_path
 			expect(page).to have_title("Ruby on Rails Tutorial Sample App | Help")
 		end
 	end
@@ -36,23 +36,35 @@ describe "StaticPages" do
 	describe "About page" do
 
 		it "should have the content 'About Us'" do
-			visit '/static_pages/about'
+			visit about_path
 			expect(page).to have_content('About Us')
 		end
 
 		it "should have the title 'About us'" do
-			visit '/static_pages/about'
+			visit about_path
 			expect(page).to have_title('Ruby on Rails Tutorial Sample App | About Us')
 		end
 	end
 
-  describe "GET /static_pages/home" do
+  	describe "GET /static_pages/home" do
 
-    it "works! (now write some real specs)" do
-      # Run the generator again with the --webrat flag if you want to use webrat methods/matchers
-      get static_pages_home_path
-      response.status.should be(200)
-    end
-  end
-  
+    	it "works! (now write some real specs)" do
+      		# Run the generator again with the --webrat flag if you want to use webrat methods/matchers
+      		get root_path
+      		response.status.should be(200)
+    	end
+	end
+
+	describe "Contact page" do
+
+		it "should have the content 'Contact'" do
+			visit contact_path
+			expect(page).to have_content('Contact')
+		end
+
+		it "should have the title 'Contact'" do
+			visit contact_path 
+			expect(page).to have_title("Ruby on Rails Tutorial Sample App | Contact")
+		end
+	end
 end
