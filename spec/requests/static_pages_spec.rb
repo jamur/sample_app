21029d468aa30dbdf8d7e15988e443a99265db50
Rafael Jamur <jamur@ufpr.br>
2014-06-10 19:51:47 -0300

    Begin improve the tests to less verbose, more objective

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -2,26 +2,35 @@ require 'spec_helper'
 
 describe "StaticPages" do
 
-	describe "Home page" do
+	subject { page }
 
+	describe "Home page" do
+		before { visit root_path }
+
+		it { should have_content('Sample App') }
+		#it { should have_title('Ruby on Rails Tutorial Sample App') }
+		it { should have_title(full_title('')) }
+		it { should_not have_title('| Home') }
+
+		=begin
 		it "should have the content 'Sample App'" do
-			visit root_path
 			expect(page).to have_content('Sample App')
 		end
 
 		it "should have the base title" do
-			visit root_path
 			expect(page).to have_title("Ruby on Rails Tutorial Sample App")
 		end
 
 		it "should not have a custom page title" do
-			visit root_path
 			expect(page).not_to have_title("| Home")
 		end
+		=end
 	end
 
 	describe "Help page" do
-
+		before { visit help_path }
+		
+		
 		it "should have the content 'Help'" do
 			visit help_path
 			expect(page).to have_content('Help')
@@ -34,6 +43,7 @@ describe "StaticPages" do
 	end
 
 	describe "About page" do
+		before { visit about_path }
 
 		it "should have the content 'About Us'" do
 			visit about_path
